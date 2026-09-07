8c5abb1a914413e33ca867a4f19691d7bef455ef
mohit9618 <pwmohit1@gmail.com>
2026-09-08 00:02:50 +0530

    Covered different types of Runnable Primietives

---
+++ LangChain Runnables/sequence_runnable.py
@@ -0,0 +1,26 @@
+from langchain_google_genai import GoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables import RunnableSequence
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+model = GoogleGenerativeAI(model = 'gemini-3.7-flash')
+
+parser = StrOutputParser()
+
+prompt2 = PromptTemplate(
+    template='Explain the following joke - {text}',
+    input_variables=['text']
+)
+
+
+chain = RunnableSequence(prompt1, model, parser, prompt2, model, parser)
+
+print(chain.invoke({'topic':'AI'}))
